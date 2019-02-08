d5c4894622fca90a01d25f807558b214bbdcfef2
Mark van den Broek <mvdnbrk@gmail.com>
2019-02-08 14:46:22 +0100

    Adds alias for place.

--- src/Resources/Author.php
+++ src/Resources/Author.php
@@ -13,4 +13,15 @@ class Author extends BaseResource
      * @var string
      */
     public $locality;
+
+    /**
+     * Alias 'place' to the 'locality' attribute.
+     *
+     * @param  string  $value
+     * @return  void
+     */
+    public function setPlaceAttribute($value)
+    {
+        $this->locality = $value;
+    }
 }
