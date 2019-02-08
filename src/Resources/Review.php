f9b58e2b2d95c679688ed9b2d1487a58d32089c2
Mark van den Broek <mvdnbrk@gmail.com>
2019-02-08 14:46:02 +0100

    Adss setters and aliases.

--- src/Resources/Review.php
+++ src/Resources/Review.php
@@ -84,4 +84,88 @@ class Review extends BaseResource
     {
         $this->id = (int) $value;
     }
+
+    /**
+     * Alias 'extra_options' to the 'meta' attribute.
+     *
+     * @param  mixed  $value
+     * @return void
+     */
+    public function setExtraOptionsAttribute($value)
+    {
+        $this->meta = $value;
+    }
+
+    /**
+     * Alias 'positive' to the 'comment_positive' attribute.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setPositiveAttribute($value)
+    {
+        $this->comment_positive = htmlspecialchars($value);
+    }
+
+    /**
+     * Alias 'negative' to the 'comment_negative' attribute.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setNegativeAttribute($value)
+    {
+        $this->comment_negative = htmlspecialchars($value);
+    }
+
+    /**
+     * Set the rating for this review.
+     *
+     * @param  int|string  $value
+     * @return void
+     */
+    public function setRatingAttribute($value)
+    {
+        $this->rating = (int) $value;
+    }
+
+    /**
+     * Alias 'reaction' to the 'respone' attribute.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setReactionAttribute($value)
+    {
+        $this->response = htmlspecialchars($value);
+    }
+
+    /**
+     * Set the recommendation for this review.
+     *
+     * @param  bool|string  $value
+     * @return void
+     */
+    public function setRecommendationAttribute($value)
+    {
+        $this->recommendation = collect([
+            'Ja' => true,
+            'Yes' => true,
+            '1' => true,
+            'Nee' => false,
+            'No' => false,
+            '0' => false,
+        ])->get($value);
+    }
+
+    /**
+     * Alias for setRatingAttribute().
+     *
+     * @param  int|string  $value
+     * @return  void
+     */
+    public function setTotalScoreAttribute($value)
+    {
+        $this->setRatingAttribute($value);
+    }
 }
