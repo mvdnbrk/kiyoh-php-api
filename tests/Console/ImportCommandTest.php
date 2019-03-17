dae5031c0376e14d8b7e3359bc382b9fb1d3757e
Mark van den Broek <mvdnbrk@gmail.com>
2019-03-17 13:46:28 +0100

    Adds import command test.

---
+++ tests/Console/ImportCommandTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mvdnbrk\Kiyoh\Tests\Console;
+
+use Mvdnbrk\Kiyoh\Models\Review;
+use Mvdnbrk\Kiyoh\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MigrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_import_reviews()
+    {
+        $this->artisan('kiyoh:import --limit=10');
+
+        $this->assertCount(10, Review::all());
+    }
+}
