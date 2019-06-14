044da7b609a8aabff024bb3295c9913312a76ec1
Mark van den Broek <mvdnbrk@gmail.com>
2019-06-14 20:06:00 +0200

    Updates kiyoh config file.

--- config/kiyoh.php
+++ config/kiyoh.php
@@ -2,7 +2,25 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | KiyOh Secret Hash
+    |--------------------------------------------------------------------------
+    |
+    | Your secret hash to connect to the KiyOh endpoint.
+    |
+    */
+
     'secret' => env('KIYOH_SECRET'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | KiyOh Reviews Table
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define the table name to store your reviews in.
+    |
+    */
 
     'table_name' => 'kiyoh_reviews',
 
